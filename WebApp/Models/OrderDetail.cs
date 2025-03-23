93c07e54fb7c0abde4f825e7dd8487ee9bf33f4e
Je-Tiev <vistaroses141@gmail.com>
2025-03-23 23:22:45 +0700

    DBtest

---
+++ WebApp/Models/OrderDetail.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Data
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
---
+++ WebApp/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Data
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required, MaxLength(255)]
+        public string PasswordHash { get; set; }
+
+        public string Role { get; set; } = "Customer";
+    }
+}
