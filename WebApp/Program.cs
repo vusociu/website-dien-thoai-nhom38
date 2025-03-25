8ac4020a815444cd8d2e772d896c9261c31ababd
vusociu <vu13102003@gmail.com>
2025-03-25 23:43:28 +0700

    add asp.net mvc

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -4,6 +4,8 @@ using Npgsql;
 //using neondb.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 builder.Services.AddOpenApi();
 
@@ -27,4 +29,15 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
---
+++ WebApp/helpers/JwtService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+public class JwtService
+{
+    private string secureKey = "this_is_a_very_secure_key_13102003";
+
+    public string generate(int id)
+    {
+        var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secureKey));
+        var credentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+        var header = new JwtHeader(credentials);
+
+        var payload = new JwtPayload(id.ToString(), null, null, null, DateTime.Today.AddDays(1));
+        var securityToken = new JwtSecurityToken(header, payload);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+
+    public JwtSecurityToken verify(string jwt)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secureKey);
+        tokenHandler.ValidateToken(jwt, new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = false,
+            ValidateAudience = false
+        }, out SecurityToken validatedToken);
+
+        return (JwtSecurityToken)validatedToken;
+    }
+}
