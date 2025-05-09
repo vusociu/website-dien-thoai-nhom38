3abd8ec44d7e9dd0d310fbf5d89c6406140ff82b
Je-Tiev <137198415+Je-Tiev@users.noreply.github.com>
2025-05-09 09:24:39 +0700

    Add interface to IProductRepository

--- WebApp/Repositories/IProductRepository.cs
+++ WebApp/Repositories/IProductRepository.cs
@@ -6,5 +6,9 @@ namespace WebApp.Repositories
     {
         List<Product> byIds(int[] ids);
         Product byId(int id);
+        List<Product> GetAll();
+        Product Create(Product product);
+        void Update(Product product);
+        void Delete(int id);
     }
 }
